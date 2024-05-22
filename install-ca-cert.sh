f90895eda0d580a3839e3bdc438adad0d6774c39
Arun Kumar Pariyar <openarungeek@gmail.com>
2024-05-23 05:42:04 +0545

    Update install-ca-cert.sh

--- install-ca-cert.sh
+++ install-ca-cert.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -e
 
-user="$(id -un 2>/dev/null || true)"
 sudo wget -O /etc/ssl/certs/ca-certificates.crt.rancher https://raw.githubusercontent.com/burmilla/os-initrd-base/master/assets/ca-certificates.crt
